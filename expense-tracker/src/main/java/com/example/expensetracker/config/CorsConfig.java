5b96eba42ade38d9dd16784e8586dc365f685bc4
Razor Tag <razortag95@gmail.com>
2026-04-26 19:27:14 -0700

    Adding a sample secret

--- expense-tracker/src/main/java/com/example/expensetracker/config/CorsConfig.java
+++ expense-tracker/src/main/java/com/example/expensetracker/config/CorsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+	
+	String AWS_KEY = "AKIAIOSFODNN7EXAMPLE";
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
